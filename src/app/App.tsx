c7429a25b0c899f66ed4bcd4e460592a723ba904
Hubert Sciechowski <h.sci@icloud.com>
2024-10-22 17:06:08 +0200

    Add prettier config file

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
-import "@/App.scss";
-import ErrorPage from "@/app/errorPage/ErrorPage";
-import Home from "@/app/home/Home";
-import Layout from "@/app/layout/Layout";
-import MyTraining from "@/app/myTraining/MyTraining";
-import Options from "@/app/options/Options";
-import Routines from "@/app/routines/Routines";
-import { AppRoutes } from "@/types/routes";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import '@/App.scss';
+import ErrorPage from '@/app/errorPage/ErrorPage';
+import Home from '@/app/home/Home';
+import Layout from '@/app/layout/Layout';
+import MyTraining from '@/app/myTraining/MyTraining';
+import Options from '@/app/options/Options';
+import Routines from '@/app/routines/Routines';
+import { AppRoutes } from '@/types/routes';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
           <Route path={AppRoutes.ROUTINES} element={<Routines />} />
           <Route path={AppRoutes.MY_TRAINING} element={<MyTraining />} />
           <Route path={AppRoutes.OPTIONS} element={<Options />} />
-          <Route path="*" element={<ErrorPage />} />
+          <Route path='*' element={<ErrorPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/app/layout/navigation/Navigation.tsx
+++ src/app/layout/navigation/Navigation.tsx
@@ -1,31 +1,24 @@
-import "./Navigation.scss";
-import HomeIcon from "@mui/icons-material/Home";
-import SettingsIcon from "@mui/icons-material/Settings";
-import NavigationItem from "@/app/layout/navigation/navigationItem/NavigationItem";
-import BorderColorIcon from "@mui/icons-material/BorderColor";
-import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
-import { AppRoutes } from "@/types/routes";
+import './Navigation.scss';
+import HomeIcon from '@mui/icons-material/Home';
+import SettingsIcon from '@mui/icons-material/Settings';
+import NavigationItem from '@/app/layout/navigation/navigationItem/NavigationItem';
+import BorderColorIcon from '@mui/icons-material/BorderColor';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import { AppRoutes } from '@/types/routes';
 
 const Navigation = () => {
   return (
-    <nav className="navigation">
-      <NavigationItem navigateTo={AppRoutes.HOME} description={"Home"}>
+    <nav className='navigation'>
+      <NavigationItem navigateTo={AppRoutes.HOME} description={'Home'}>
         <HomeIcon />
       </NavigationItem>
-      <NavigationItem navigateTo={AppRoutes.ROUTINES} description={"Routines"}>
+      <NavigationItem navigateTo={AppRoutes.ROUTINES} description={'Routines'}>
         <BorderColorIcon />
       </NavigationItem>
-      <NavigationItem
-        navigateTo={AppRoutes.MY_TRAINING}
-        description={"My trainings"}
-      >
+      <NavigationItem navigateTo={AppRoutes.MY_TRAINING} description={'My trainings'}>
         <FitnessCenterIcon />
       </NavigationItem>
-      <NavigationItem
-        className="navigaton_options"
-        navigateTo={AppRoutes.OPTIONS}
-        description={"Options"}
-      >
+      <NavigationItem className='navigaton_options' navigateTo={AppRoutes.OPTIONS} description={'Options'}>
         <SettingsIcon />
       </NavigationItem>
     </nav>
