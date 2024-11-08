e131606961b1ce06b1d618375df38b029392ae7e
PiotrekBaranowski <piotrekbaranowski95@gmail.com>
2024-11-08 19:32:40 +0100

    styling page

--- src/app/workouts/exerciseCard/ExerciseCard.tsx
+++ src/app/workouts/exerciseCard/ExerciseCard.tsx
@@ -1,35 +1,80 @@
 import React from 'react';
 import { WorkoutExercise } from '@/types/entities/Workout.ts';
-import { Typography, Divider, List, ListItem, ListItemText } from '@mui/material';
+import { Typography, Divider, List, ListItem, ListItemText, TextField, InputAdornment } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 import './ExerciseCard.scss';
+import Button from '@/app/components/buttons/Button/Button';
 
 type ExerciseCardProps = {
   exercise: WorkoutExercise;
 };
 
 const ExerciseCard: React.FC<ExerciseCardProps> = ({ exercise }) => {
+  const handleDeleteExercise = () => {
+    alert('exercise deleted' + exercise.workoutExerciseId);
+  };
+
+  const handleDeleteSet = (id: string | null) => {
+    alert('set deleted' + id);
+  };
+
+  const handleAddSet = () => {
+    alert('set added');
+  };
+
   return (
     <div className='exercise-card'>
-      <Typography variant='h6' gutterBottom>
-        {exercise.exercise.name}
-      </Typography>
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Typography variant='h6' gutterBottom>
+          {exercise.exercise.name}{' '}
+        </Typography>
+        <CloseIcon onClick={handleDeleteExercise} style={{ cursor: 'pointer' }} />
+      </div>
+
       <Divider className='divider' />
       <List>
         {exercise.sets.map((set, setIndex) => (
-          <ListItem key={setIndex}>
-            <ListItemText
-              primary={<Typography variant='subtitle2'>Set {set.setNumber}</Typography>}
-              secondary={
-                <Typography variant='body2'>
-                  <strong>Reps:</strong> {set.reps || 'N/A'}, <strong>Weight:</strong>{' '}
-                  {set.weight || 'N/A'}, <strong>Rest Time:</strong> {set.restTime || 'N/A'}
-                </Typography>
-              }
-            />
+          <ListItem key={set.setNumber}>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+              <div>set {setIndex + 1} </div>
+              <TextField sx={{ width: '75px' }} label='reps' defaultValue={set.reps} />
+              <TextField
+                sx={{ width: '75px' }}
+                label='weight'
+                defaultValue={set.weight}
+                slotProps={{
+                  input: {
+                    endAdornment: (
+                      <InputAdornment className='helper-text' position='end' style={{ color: '#6c757d' }}>
+                        <span className='helper-text'>kg</span>
+                      </InputAdornment>
+                    ),
+                  },
+                }}
+              />
+              <TextField
+                sx={{ width: '75px' }}
+                label='rest time'
+                defaultValue={set.restTime}
+                slotProps={{
+                  input: {
+                    endAdornment: (
+                      <InputAdornment position='end'>
+                        <span className='helper-text'>s</span>
+                      </InputAdornment>
+                    ),
+                  },
+                }}
+              />
+              {setIndex !== 0 && (
+                <CloseIcon style={{ cursor: 'pointer' }} onClick={() => handleDeleteSet(set.workoutExerciseSetId)} />
+              )}
+            </div>
           </ListItem>
         ))}
       </List>
+      <Button onClick={handleAddSet}>Add new set</Button>
       <Divider className='divider' />
       <Typography variant='body2' className='notes'>
         Notes: {exercise.notes || 'None'}
--- src/app/workouts/workoutCreate/WorkoutCreate.tsx
+++ src/app/workouts/workoutCreate/WorkoutCreate.tsx
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { Workout } from '@/types/entities/Workout';
 import { RoutineExercise } from '@/types/entities/Routine';
 import { createWorkout } from '@/api/workoutApi';
-import { AppRoutes } from '@/types/routes';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { Typography } from '@mui/material';
 import Button from '@/app/components/buttons/Button/Button.tsx';
 import ExerciseCard from '../exerciseCard/ExerciseCard.tsx';
-
+import dayjs from 'dayjs';
 import './WorkoutCreate.scss';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { TextField } from '@mui/material';
+import updateLocale from 'dayjs/plugin/updateLocale';
+
+dayjs.extend(updateLocale);
+dayjs.updateLocale('en', {
+  weekStart: 1,
+});
 
 const WorkoutCreate: React.FC = () => {
   const location = useLocation();
-  const navigate = useNavigate();
   const [workout, setWorkout] = useState<Workout | null>(null);
+  const [selectedDate, setSelectedDate] = React.useState(workout?.date ?? dayjs());
 
   useEffect(() => {
     const routineData = location.state?.routineData;
@@ -25,21 +34,18 @@ const WorkoutCreate: React.FC = () => {
         comment: '',
         routineId: routineData.routine.id,
         exercises: routineData.exercises.map((routineExercise: RoutineExercise) => ({
-          workoutExerciseId: null,
+          workoutExerciseId: 'testID',
           exercise: routineExercise.exercise,
           orderNumber: routineExercise.orderNumber,
           notes: routineExercise.notes || '',
-          sets: Array.from(
-            { length: routineExercise.defaultsSets || 1 },
-            (_, index) => ({
-              workoutExerciseSetId: null,
-              setNumber: index + 1,
-              reps: routineExercise.defaultReps,
-              weight: routineExercise.defaultWeight,
-              restTime: routineExercise.defaultRestTime,
-              comment: '',
-            })
-          ),
+          sets: Array.from({ length: routineExercise.defaultsSets || 1 }, (_, index) => ({
+            workoutExerciseSetId: 'testID',
+            setNumber: index + 1,
+            reps: routineExercise.defaultReps,
+            weight: routineExercise.defaultWeight,
+            restTime: routineExercise.defaultRestTime,
+            comment: '',
+          })),
         })),
       };
       setWorkout(initialWorkout);
@@ -64,38 +70,78 @@ const WorkoutCreate: React.FC = () => {
     return <div>Loading...</div>;
   }
 
-  const sortedExercises = [...workout.exercises].sort(
-    (a, b) => a.orderNumber - b.orderNumber
-  );
+  const sortedExercises = [...workout.exercises].sort((a, b) => a.orderNumber - b.orderNumber);
+
+  const handleDateChange = (newDate) => {
+    setSelectedDate(newDate);
+  };
 
   return (
     <div className='workout-create'>
       <div className='workout-create-header'>
         <Typography variant='h4' gutterBottom>
-          Create Workout from Routine
+          New Workout
         </Typography>
         <Button onClick={handleCreateWorkout} className='create-workout-button' size='big'>
           CREATE WORKOUT
         </Button>
       </div>
-
-      <Typography variant='h6'>Workout Name:</Typography>
-      <Typography variant='body1'>{workout.name}</Typography>
-
-      <Typography variant='h6' style={{ marginTop: '16px' }}>
-        Date:
-      </Typography>
-      <Typography variant='body1'>{workout.date.toDateString()}</Typography>
-
-      {workout.comment && (
-        <>
-          <Typography variant='h6' style={{ marginTop: '16px' }}>
-            Comment:
-          </Typography>
-          <Typography variant='body1'>{workout.comment}</Typography>
-        </>
-      )}
-
+      <div className='workout-create-info'>
+        <div className='name-and-date'>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              className='calendar'
+              label='Workout Date'
+              value={selectedDate}
+              onChange={handleDateChange}
+              format='DD/MM/YYYY'
+              // TODO CHANGE IT FROM SLOT PROPS
+              slotProps={{
+                desktopPaper: {
+                  sx: {
+                    backgroundColor: '#181818;', // Change the calendar background color
+                  },
+                },
+                mobilePaper: {
+                  sx: {
+                    backgroundColor: '#181818;', // Mobile-specific background color
+                  },
+                },
+                day: {
+                  sx: {
+                    '&.Mui-selected': {
+                      backgroundColor: '#3f454b', // Selected day background
+                      color: 'white', // Selected day text color
+                    },
+                    '&:not(.Mui-selected)': {
+                      color: '#6c757d', // Unselected day color
+                    },
+                  },
+                },
+                switchViewButton: {
+                  sx: {
+                    color: '#6c757d', // Month selection arrow color
+                  },
+                },
+                previousIconButton: {
+                  sx: {
+                    color: '#6c757d', // Left arrow color
+                  },
+                },
+                nextIconButton: {
+                  sx: {
+                    color: '#6c757d', // Right arrow color
+                  },
+                },
+              }}
+            />
+          </LocalizationProvider>
+          <TextField label='Workout name' defaultValue={workout.name} />
+        </div>
+        <div className='comment'>
+          <TextField label='Comment' defaultValue={workout.comment} />
+        </div>
+      </div>
       <Typography variant='h6' style={{ marginTop: '16px' }}>
         Exercises:
       </Typography>
