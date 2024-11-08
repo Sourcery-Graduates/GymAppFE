3e40ce1aefbb7287e11835fac3e6dcf1492ded7c
PiotrekBaranowski <piotrekbaranowski95@gmail.com>
2024-11-08 15:31:54 +0100

    added functionality of my-trainings, need to edit styles

---
+++ src/types/entities/Workout.ts
@@ -0,0 +1,26 @@
+import { SimpleExercise } from '@/types/entities/Routine';
+
+export interface Workout {
+  id: string;
+  name: string;
+  date: Date;
+  comment: string;
+  exercises: WorkoutExercise[];
+}
+
+export interface WorkoutExercise {
+  id: string;
+  exercise: SimpleExercise;
+  orderNumber: number;
+  notes: string;
+  sets: WorkoutExerciseSet[];
+}
+
+export interface WorkoutExerciseSet {
+  id: string;
+  setNumber: number;
+  reps: number;
+  weight: number;
+  restTime: number;
+  comment: string;
+}
