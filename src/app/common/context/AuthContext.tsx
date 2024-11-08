cf1c5b51389673d3819c52ec35ecf0463371f034
PiotrekBaranowski <piotrekbaranowski95@gmail.com>
2024-11-08 12:04:17 +0100

    Added clearing of queryClient on loggin out

--- src/app/common/context/AuthContext.tsx
+++ src/app/common/context/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { AuthState } from '@/types/auth';
+import { useQueryClient } from '@tanstack/react-query';
 import React, { createContext, useState } from 'react';
 
 interface AuthContextProps {
@@ -9,10 +10,12 @@ export const AuthContext = createContext<AuthState>({} as AuthState);
 export const AuthContextProvider = ({ children }: AuthContextProps): JSX.Element => {
   const token = localStorage.getItem('token') ?? sessionStorage.getItem('token') ?? null;
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(token !== null);
+  const queryClient = useQueryClient();
 
   const logOutUser = (): void => {
     localStorage.removeItem('token');
     sessionStorage.removeItem('token');
+    queryClient.clear();
     setIsLoggedIn(false);
   };
 
