309f74ac0603ffe11442df665f05d106503c3650
Valentyn Bondarenko <murica.it@gmail.com>
2024-12-04 15:02:14 +0100

    Bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
 	plugins: [react()],
-	base: mode === 'production' ? '/GymAppFE/' : '/',
+	base: mode === 'production' ? '/' : '/',
 	server: {
 		port: 3000,
 	},
