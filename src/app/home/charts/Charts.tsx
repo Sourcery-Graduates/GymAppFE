1cdddbcb911be224b115acf706d8c854cf0848ac
przemyslawren <przemyslaw.ren@gmail.com>
2025-01-16 12:12:41 +0100

    add muscle group chart with mock data

--- src/app/home/charts/Charts.tsx
+++ src/app/home/charts/Charts.tsx
@@ -1,15 +1,56 @@
-import { BarChart } from '@mui/x-charts';
+import { PieChart } from '@mui/x-charts';
+import './Charts.scss';
+import { useEffect, useState } from 'react';
+import Box from '@mui/material/Box';
 
 const Charts = () => {
+  const [isLegendHidden, setIsLegendHidden] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 800) {
+        setIsLegendHidden(true);
+      } else {
+        setIsLegendHidden(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className='muscle-group-chart'>
-      <h2 className='muscle-group-chart__title'>Favourite muscles</h2>
-      <BarChart
-        xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
-        series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
-        width={340}
-        height={300}
-      />
+      <h2 className='muscle-group-chart__title'>Muscle sets per week</h2>
+      <Box className='muscle-group-chart__content'>
+        <PieChart
+          slotProps={{
+            legend: { hidden: isLegendHidden },
+          }}
+          series={[
+            {
+              data: [
+                { id: 1, value: 7, label: 'middleback' },
+                { id: 2, value: 12, label: 'lats' },
+                { id: 3, value: 18, label: 'abdominals' },
+                { id: 4, value: 22, label: 'lowerback' },
+                { id: 5, value: 16, label: 'forearms' },
+                { id: 6, value: 30, label: 'abductors' },
+                { id: 7, value: 27, label: 'traps' },
+                { id: 8, value: 20, label: 'hamstrings' },
+                { id: 9, value: 6, label: 'quadriceps' },
+                { id: 10, value: 23, label: 'chest' },
+                { id: 11, value: 22, label: 'biceps' },
+              ],
+              innerRadius: 15,
+              paddingAngle: 3,
+              cornerRadius: 3,
+              highlightScope: { fade: 'global', highlight: 'item' },
+              faded: { innerRadius: 30, additionalRadius: -30, color: '#181818' },
+            },
+          ]}
+        />
+      </Box>
     </div>
   );
 };
