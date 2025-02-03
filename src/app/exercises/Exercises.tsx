21608613348e917b61408b78ee1eb7b7e740cc62
przemyslawren <przemyslaw.ren@gmail.com>
2025-02-03 14:31:37 +0100

    update exercises

--- src/app/exercises/Exercises.tsx
+++ src/app/exercises/Exercises.tsx
@@ -1,48 +1,22 @@
-import { fetchExercisePagedByName } from '@/api/exerciseApi';
-import { useQuery } from '@tanstack/react-query';
 import SearchIcon from '@mui/icons-material/Search';
-import { useState } from 'react';
 import ExerciseList from './exerciseList/ExerciseList';
-import BasicSpinner from '../components/loaders/BasicSpinner';
-import ErrorPage from '../errorPage/ErrorPage';
+import { useRef, useState } from 'react';
+import debounce from '@mui/material/utils/debounce';
 import './Exercises.scss';
-import { debounce } from '../common/utils/debounce';
-import TablePagination from '@mui/material/TablePagination/TablePagination';
 
 const Exercises = () => {
   const [searchValue, setSearchValue] = useState<string>('');
-  const [rowsPerPage, setRowsPerPage] = useState<number>(25);
-  const [currentPage, setCurrentPage] = useState<number>(0);
-
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ['exercises', searchValue, currentPage, rowsPerPage],
-    queryFn: () => fetchExercisePagedByName(searchValue, currentPage, rowsPerPage),
-  });
+  const scrollRef = useRef<HTMLDivElement>(null);
 
   const debouncedHandleSearch = debounce((event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(event.target.value);
   }, 300);
 
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    debouncedHandleSearch(event);
+  const scrollTop = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
   };
-
-  const handlePageChange = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
-    setCurrentPage(newPage);
-  };
-
-  const handleRowsPerPageChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setCurrentPage(0);
-  };
-
-  if (isLoading || !data) {
-    return <BasicSpinner />;
-  }
-
-  if (isError) {
-    return <ErrorPage />;
-  }
 
   return (
     <div className='exercises'>
@@ -54,24 +28,11 @@ const Exercises = () => {
           className='exercises__search-bar--field'
           type='search'
           placeholder='Search exercises...'
-          onChange={handleSearch}
+          onChange={debouncedHandleSearch}
         />
       </form>
-      <div className='exercises__list'>
-        <ExerciseList exercises={data.data} />
-      </div>
-      <div className='exercises-pagination'>
-        <TablePagination
-          component='div'
-          count={data.totalElements}
-          page={currentPage}
-          onPageChange={handlePageChange}
-          rowsPerPage={rowsPerPage}
-          onRowsPerPageChange={handleRowsPerPageChange}
-          rowsPerPageOptions={[10, 25, 50, 100]}
-          labelRowsPerPage='Rows:'
-          className='exercisesPagination'
-        />
+      <div className='exercises__list' ref={scrollRef}>
+        <ExerciseList scrollTop={scrollTop} searchValue={searchValue} />
       </div>
     </div>
   );
