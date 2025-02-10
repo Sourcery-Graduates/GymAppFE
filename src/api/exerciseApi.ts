2b2676cddb5c2e080dfb881cf9beb74127ef1ff8
Przemek <122983696+przemyslawren@users.noreply.github.com>
2025-02-10 11:09:54 +0100

    Feature/gym 149 fe create exercise database (#45)
    * add exercise db
    
    * update styling for consistency
    
    * update imports
    
    * update exercises
    
    * add filtering by primary muscle, update image ux
    
    * add consistent search text fields
    
    * update styling
    
    * update layout
    
    * update exercise list display
    
    * add flipping list item
    
    * update styles
    
    * update layout
    
    * finish

--- src/api/exerciseApi.ts
+++ src/api/exerciseApi.ts
@@ -1,14 +1,34 @@
 import api from '@/config/axios/config';
-import { ExerciseDetails } from '@/types/entities/Exercise';
+import { ExerciseDetails, ExerciseDetailsPage } from '@/types/entities/Exercise';
 
-const exerciseSearchApi = '/api/workout/exercise';
+export enum EXERCISE_Endpoint {
+  EXERCISE_BY_PREFIX = 'api/workout/exercise',
+  EXERCISE_BY_PREFIX_AND_PRIMARY_MUSCLE = 'api/workout/exercise/primary-muscle',
+}
 
 export const fetchExerciseByName = async (exercisePrefix: string = ''): Promise<ExerciseDetails[]> => {
   const size = 10;
   const page = 0;
 
-  const response = await api.get(exerciseSearchApi + `?page=${page}&size=${size}&prefix=${exercisePrefix}`);
+  const response = await api.get(
+    EXERCISE_Endpoint.EXERCISE_BY_PREFIX + `?page=${page}&size=${size}&prefix=${exercisePrefix}`,
+  );
   const exerciseDetails: ExerciseDetails[] = response.data.data;
+
+  return exerciseDetails;
+};
+
+export const fetchExercisePagedByPrimaryMuscleAndPrefix = async (
+  exercisePrefix: string = '',
+  primaryMuscle: string = '',
+  page: number = 0,
+  size: number = 10,
+): Promise<ExerciseDetailsPage> => {
+  const response = await api.get(
+    EXERCISE_Endpoint.EXERCISE_BY_PREFIX_AND_PRIMARY_MUSCLE +
+      `?page=${page}&size=${size}&prefix=${exercisePrefix}&primaryMuscle=${primaryMuscle}`,
+  );
+  const exerciseDetails: ExerciseDetailsPage = response.data;
 
   return exerciseDetails;
 };
