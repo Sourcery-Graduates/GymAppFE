68091b11d154bd4aa0a58843b773b4516bde224a
Piotr Kosacz <piotr.kosacz1@gmail.com>
2025-05-20 15:05:58 +0200

    GYM-208: add first playwright test

---
+++ pages/login.page.ts
@@ -0,0 +1,16 @@
+import { Locator, Page } from "@playwright/test";
+
+export class LoginPage {
+    
+    emailInput: Locator;
+    passwordInput: Locator;
+    loginButton: Locator;
+    credentialsError: Locator
+
+    constructor(private page: Page) {
+        this.emailInput = this.page.getByRole('textbox', { name: 'Email' });
+        this.passwordInput = this.page.getByRole('textbox', { name: 'Password' });
+        this.loginButton = this.page.getByRole('button', { name: 'Login' });
+        this.credentialsError = this.page.locator('#credentials-error-toast > div > span')
+    }
+}
