42e39a2099c95de979a816b2a1d77e7a0d995f35
pkosacz <piotr.kosacz1@gmail.com>
2025-05-21 16:26:04 +0200

    GYM-208: add first playwright test (#53)
    * GYM-208: add first playwright test
    
    * GYM-208: change direcotries paths

---
+++ e2e/tests/login.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/login.page';
+
+test.describe("User login to GymApp", async () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  })
+
+  test('unsuccessful login with non-existing user', async ({ page }) => {
+    // Arrange
+    const loginPage = new LoginPage(page);
+    const nonExistingEmail = 'user@test.com';
+    const nonExistingPassword = 'password'; 
+    const expectedErrorMessage = 'Invalid email or password. Please try again.'
+
+    // Act
+    await loginPage.emailInput.fill(nonExistingEmail);
+    await loginPage.passwordInput.fill(nonExistingPassword);
+    await loginPage.loginButton.click();
+
+    // Assert
+    await expect(loginPage.credentialsError).toHaveText(expectedErrorMessage);
+  });
+});
