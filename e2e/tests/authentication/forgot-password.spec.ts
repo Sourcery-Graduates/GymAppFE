380789e09686e00c0fbe47962b442a82f6119c48
Piotr Kosacz <piotr.kosacz1@gmail.com>
2025-06-16 15:11:53 +0200

    GYM-215: remove unused import and fix typo

--- e2e/tests/authentication/forgot-password.spec.ts
+++ e2e/tests/authentication/forgot-password.spec.ts
@@ -1,28 +1,28 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import { LoginPage } from '../../pages/login.page';
 import { ForgotPasswordPage } from '../../pages/forgot-password.page';
 
 test.describe('User forgot the password', async () => {
   let loginPage: LoginPage;
-  let forgotPassowrdPage: ForgotPasswordPage;
+  let forgotPasswordPage: ForgotPasswordPage;
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page);
-    forgotPassowrdPage = new ForgotPasswordPage(page);
+    forgotPasswordPage = new ForgotPasswordPage(page);
     await loginPage.gotoForgotPasswordPage();
   });
 
   test('should navigate to forgot password page when link is clicked', async () => {
     // Assert
-    await forgotPassowrdPage.expectToBeOnForgotPasswordPage();
+    await forgotPasswordPage.expectToBeOnForgotPasswordPage();
   });
 
   test('should display error message when email field is empty', async () => {
     // Assert
-    await forgotPassowrdPage.expectErrorMessage();
-  })
+    await forgotPasswordPage.expectErrorMessage();
+  });
   test('should navigate to login page when return link is clicked', async () => {
     // Act
-    await forgotPassowrdPage.returnToLoginPage();
+    await forgotPasswordPage.returnToLoginPage();
     // Assert
     await loginPage.expectToBeOnLoginPage();
   });
