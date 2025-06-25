bc0e3744b3499d8a46b8ba48e4e3cfc13b204d77
Agnieszka Biedrzycka <aga.pedlewska@gmail.com>
2025-06-25 12:31:03 +0200

    GYM-242: Add routine in test

--- e2e/tests/application/workout.spec.ts
+++ e2e/tests/application/workout.spec.ts
@@ -7,6 +7,8 @@ import { MyTrainingPage } from '../../pages/my-training.page';
 import { RoutineHelper } from '../../helpers/routineHelper';
 import { sandbagLoadWorkout } from '../../test-data/workout.data';
 import { strengthStabilityRoutine } from '../../test-data/routine.data';
+import { RoutinesPage } from '../../pages/routines.page';
+import { RoutineDetailsPage } from '../../pages/routine-details.page';
 
 test.describe('User with existing workouts', async () => {
   let apiContext: APIRequestContext;
@@ -72,5 +74,43 @@ test.describe('User with existing workouts', async () => {
     await workoutPage.expectExerciseToHaveSetCount(exerciseName, updatedSetCount);
 
     await workoutHelper.deleteWorkout(workout.id, workout.routineId);
+  });
+});
+
+test.describe('User with no workouts', async () => {
+  let apiContext: APIRequestContext;
+  let workoutPage: WorkoutPage;
+  let routinePage: RoutinesPage;
+  let routineDetailsPage: RoutineDetailsPage;
+
+  test.beforeAll(async () => {
+    apiContext = await createApiContextFromStorageState('./e2e/.auth/user.json');
+  });
+
+  test.afterAll(async () => {
+    await apiContext.dispose();
+  });
+
+  test.beforeEach(async ({ page }) => {
+    workoutPage = new WorkoutPage(page);
+    routinePage = new RoutinesPage(page);
+    routineDetailsPage = new RoutineDetailsPage(page);
+  });
+
+  test.only('can create workout from existing routine', async () => {
+    const routineHelper = new RoutineHelper(apiContext);
+    const exerciseHelper = new ExerciseHelper(apiContext);
+    const routineName = strengthStabilityRoutine.name;
+    const routineDesc = strengthStabilityRoutine.description;
+
+    const exercise = await exerciseHelper.getGivenNumberOfExercises(2);
+    const routine = await routineHelper.createRoutine(routineName, routineDesc);
+    await exerciseHelper.addExercisesToRoutine(routine.id, exercise);
+    await routinePage.goto();
+    await routinePage.expectHeadingToBeVisible();
+
+    await routinePage.goToRoutineDetails();
+
+    // await workoutHelper.deleteWorkout(workout.id, workout.routineId);
   });
 });
